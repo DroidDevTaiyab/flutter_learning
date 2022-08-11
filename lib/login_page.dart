5b0dbefe4fb6b42aabea37916aaa280ae65ce61c
ali <taiyab@bitlasoft.com>
2022-08-12 02:08:08 +0530

    code formatted

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,15 +9,9 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        
-        
-      child: Column(    
-          children: [
-          
+        child: Column(children: [
           Image.asset("assets/images/login_image.png"),
-          
           SizedBox(height: 20.0),
-          
           Text(
             "Welcome",
             style: TextStyle(
@@ -25,29 +19,24 @@ class LoginPage extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          
           SizedBox(height: 20.0),
-          
           Padding(
-            padding:const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
             child: Column(
               children: [
                 TextFormField(
                   decoration: InputDecoration(
                       hintText: "Enter User Name", labelText: "User Name"),
                 ),
-            
                 TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
                       hintText: "Enter Password", labelText: "Password"),
                 ),
-            
                 SizedBox(
                   height: 20.0,
                 ),
-            
                 ElevatedButton(
                   onPressed: () {
                     print("Button Clicked");
